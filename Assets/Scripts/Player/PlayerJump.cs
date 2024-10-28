cd8eac154789cd3129ffca27169532de7318bff2
erine125 <catherine.wang.qq@gmail.com>
2024-10-27 21:45:13 -0700

    added town bridge switch

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -27,7 +27,7 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rb.velocity.y);
+        //Debug.Log(rb.velocity.y);
         if (IsGrounded())
         {
             animator.SetBool("isGrounded", true);
